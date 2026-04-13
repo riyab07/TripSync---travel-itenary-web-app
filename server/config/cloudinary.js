4b208fab11d41ce70925b25dad9311b3cd83be22
unknown <riyavishalbhardwaj@gmail.com>
2026-04-13 21:47:33 +0530

    fix cloudinary env variable bug

--- server/config/cloudinary.js
+++ server/config/cloudinary.js
@@ -1,9 +1,9 @@
 import { v2 as cloudinary } from 'cloudinary';
 
 cloudinary.config({
-  cloud_name: process.env.ddhqak4f0,
-  api_key: process.env.273558521294738,
-  api_secret: process.env.SJADeLowrkCT2oiQOE2kc8yEHfk,
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
 export default cloudinary;
